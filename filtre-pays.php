a69664f149ee8f6ed720da1395d5ae08c3252cb9
Laura-Jeanne Fournier L <e2495693@cmaisonneuve.qc.ca>
2024-12-19 11:05:11 -0500

    Modifer le code pour debugger

--- filtre-pays.php
+++ filtre-pays.php
@@ -6,6 +6,17 @@ Version: 1.0
 Author: Laura-Jeanne Fournier
 */
 
+function filtre_pays_enqueue_scripts() {
+    wp_enqueue_script(
+        'filtre-pays-script',
+        plugin_dir_url(__FILE__) . 'js/filtre-pays.js',
+        ['jquery'],
+        filemtime(plugin_dir_path(__FILE__) . 'js/filtre-pays.js'),
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'filtre_pays_enqueue_scripts');
+
 function filtre_pays_register_rest_route() {
     register_rest_route('filtre-pays/v1', '/destinations', [
         'methods' => 'GET',
@@ -18,8 +29,19 @@ function filtre_pays_register_rest_route() {
         ],
     ]);
 }
-add_action('rest_api_init', 'filtre_pays_register_rest_route');
-
+add_action('rest_api_init', function () {
+    error_log('REST API initialisée pour filtre-pays');
+    register_rest_route('filtre-pays/v1', '/destinations', [
+        'methods' => 'GET',
+        'callback' => 'filtre_pays_get_destinations',
+        'args' => [
+            'pays' => [
+                'required' => true,
+                'sanitize_callback' => 'sanitize_text_field',
+            ],
+        ],
+    ]);
+});
 function filtre_pays_get_destinations($request) {
     $pays = $request->get_param('pays');
     $query = new WP_Query([
